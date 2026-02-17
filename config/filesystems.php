172137a5cb0d81bc9ba27e765cbd39f52f9ca4fd
Ally Prof <allyprof@example.com>
2026-02-17 20:19:16 +0300

    Switch to direct 'public/storage' configuration to resolve cPanel 404s

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,9 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => env('APP_ENV') === 'production' 
-                ? '/home/primkgek/public_html/storage' 
-                : public_path('storage'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
