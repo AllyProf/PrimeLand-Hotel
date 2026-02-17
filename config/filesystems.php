450612b61f2cfedee883b8062d7b9dce7f784f76
Ally Prof <allyprof@example.com>
2026-02-17 19:59:32 +0300

    Configure direct storage path for cPanel (no symlink needed)

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => env('APP_ENV') === 'production' 
+                ? '/home/primkgek/public_html/storage' 
+                : public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
