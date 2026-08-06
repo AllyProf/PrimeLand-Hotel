84600be763f7e8e5a08f7d637e5f8982448297ee
Ally Prof <allyprof@example.com>
2026-08-06 14:19:43 +0300

    Update hero section Book Now button to point to online booking portal

--- resources/views/landing_page_views/index.blade.php
+++ resources/views/landing_page_views/index.blade.php
@@ -37,7 +37,7 @@
             </p>
 
             <div class="hero-button wow fadeInUp" data-wow-delay=".7s">
-                <a href="{{ url('/contact') }}" class="gt-theme-btn">Book Now</a>
+                <a href="{{ config('services.aiosell.booking_url') }}" target="_blank" class="gt-theme-btn">Book Now</a>
                 <a href="{{ url('/rooms') }}" class="gt-theme-btn style-2">Our Rooms</a>
             </div>
         </div>
